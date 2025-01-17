2cb9c22f51768a8cd914e0304d55c0786bf61f39
dantabarka <dan.tabarka@gmail.com>
2025-01-17 13:17:50 +0100

    corected printing and added 'add' function

--- InfinityFLoat.py
+++ InfinityFLoat.py
@@ -16,21 +16,28 @@ class InfinityFLoat:
                               represents the number 3.14 (314 * 10^(-2)).
     """
     def __init__(self, number: int, exponent_shift: int):
-        self.number, self.exp = self.base_form(number, exponent_shift)
+        self.base_form(number, exponent_shift)
     
-    def base_form(self, number: int, exponent_shift: int) -> tuple[int, int]:
+    def base_form(self, number: int, exponent_shift: int) -> None:
         while number % 10 == 0:
             number //= 10
             exponent_shift += 1
-        return number, exponent_shift
+        self.number = number
+        self.exp = exponent_shift
     
-    def add(self, float: 'InfinityFLoat') -> 'InfinityFLoat':
+    def add(self, other: 'InfinityFLoat') -> 'InfinityFLoat':
+        min_exp = min(self.exp, other.exp)
+        self.set_exponent(min_exp)
+        other.set_exponent(min_exp)
+
+        integral_part = self.number + other.number
+        return InfinityFLoat(integral_part, min_exp)
+
+    def sub(self, other: 'InfinityFLoat') -> 'InfinityFLoat':
         pass
-    def sub(self, float: 'InfinityFLoat') -> 'InfinityFLoat':
+    def mul(self, other: 'InfinityFLoat') -> 'InfinityFLoat':
         pass
-    def mul(self, float: 'InfinityFLoat') -> 'InfinityFLoat':
-        pass
-    def div(self, float: 'InfinityFLoat') -> 'InfinityFLoat':
+    def div(self, other: 'InfinityFLoat') -> 'InfinityFLoat':
         pass
     
     # helper funcions __________________________________________________________________________
@@ -39,27 +46,51 @@ class InfinityFLoat:
         while number > 0:
             count += 1
             number //= 10
-        return count 
-
+        return count
+    
+    def set_exponent(self, exp: int) -> None:
+        dif = self.exp - exp
+        self.exp = exp
+        self.number *= 10**dif
+    
+    # magic methonds ___________________________________________________________________________
     def __str__(self):
         if self.exp >= 0:
             return f'{self.number} * 10^{self.exp} => {self.number * 10**(self.exp)}'
         
         exp = -self.exp
         integral_part = self.number // (10**exp)
-        zeros = "0"*(exp - self.digit_count(self.number))
         decimal_part = self.number - (integral_part * (10**exp))
+        zeros = "0"*(exp - self.digit_count(decimal_part))
 
         return f'{self.number} * 10^{self.exp} => {integral_part},{zeros}{decimal_part}'
 
 
-my_float = InfinityFLoat(314, -2)
-print(my_float)
-my_float = InfinityFLoat(314, 2)
-print(my_float)
-my_float = InfinityFLoat(31415926535, -100)
-print(my_float)
+fl = InfinityFLoat(5, -2)
+print(fl)
+fl = InfinityFLoat(5, -1)
+print(fl)
+fl = InfinityFLoat(5, 0)
+print(fl)
+fl = InfinityFLoat(123456, -3)
+print(fl)
+fl = InfinityFLoat(123000456, -3)
+print(fl)
+fl = InfinityFLoat(123000456, -6)
+print(fl)
+fl = InfinityFLoat(123000456, -4)
+print(fl)
 
-my_float = InfinityFLoat(30000, 0)
-print(my_float)
+print("__add testing__")
 
+small = InfinityFLoat(5, -2)
+big = InfinityFLoat(3, 2)
+
+another = small.add(big)
+print(another)
+
+small = InfinityFLoat(5, 5)
+big = InfinityFLoat(123456, 10)
+
+another = small.add(big)
+print(another)
